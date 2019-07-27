4848413542e5f9a46610fd4f44ca3af760c56ed8
jleon <jleon@psicol.co>
2019-07-27 10:15:21 -0500

    primer commit

---
+++ database/migrations/2019_07_27_145545_create_announces_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnouncesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announces', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('description');
+            $table->integer('product_id');
+            $table->tinyInteger('status')->default(1);
+            $table->date('sold_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announces');
+    }
+}
