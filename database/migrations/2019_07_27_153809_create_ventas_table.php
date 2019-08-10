81b1b5dd1c98de8bfa5c133b135bc3bcebc8935e
Santiago Moreno <smoreno@psicol.co>
2019-08-10 09:15:13 -0500

    Commit inicial

---
+++ database/migrations/2019_07_27_153809_create_ventas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('anuncio_id');
+            $table->integer('status');
+            $table->date('sold_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Este es el incio</title>
+</head>
+<body>
+	<a href="https://psicol.co" target="_blank">Conócenos</a>
+</body>
+</html>
